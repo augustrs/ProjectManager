1914c3f6b9b925f8ed21654c9ba821437172d407
augustrs <auso0001@stud.kea.dk>
2024-04-26 11:50:30 +0200

    added project

---
+++ src/test/java/com/example/projectmanagerkea/ProjectManagerKeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagerkea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerKeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
