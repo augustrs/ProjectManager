167267371ec130f83466ce904b041023457d56eb
augustrs <auso0001@stud.kea.dk>
2024-05-10 12:50:53 +0200

    refactored controller

--- src/main/java/com/example/projectmanagerkea/controller/ProjectController.java
+++ src/main/java/com/example/projectmanagerkea/controller/ProjectController.java
@@ -28,92 +28,7 @@ public class ProjectController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public String homeScreen() {
-        return "redirect:/login";
-    }
 
-    @GetMapping("/login")
-    public String login() {
-        return "index";
-    }
-
-    @PostMapping("/login")
-    public String loginSubmit(@RequestParam String username, @RequestParam String password, HttpSession session, Model model) {
-        try {
-            User user = userService.verifyLogIn(username, password);
-            if (user != null && user.getRoleId() == 1) {
-                session.setAttribute("loggedInUser", user);
-
-                return "redirect:/admin";
-            } else if (user != null && user.getRoleId() == 3) {
-                session.setAttribute("loggedInUser", user);
-
-                return "redirect:/dashboard";
-            } else if (user != null && user.getRoleId() == 2) {
-                session.setAttribute("loggedInUser", user);
-
-                return "redirect:/managerDashboard";
-            } else {
-                model.addAttribute("error", "invalid credentials");
-                return "index";
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @GetMapping("/admin")
-    public String admin(HttpSession session, Model model) {
-        User loggedInUser = (User) session.getAttribute("loggedInUser");
-        if (loggedInUser != null && loggedInUser.getRoleId() == 1) {
-            return "admin";
-        } else if ((loggedInUser != null && loggedInUser.getRoleId() == 3)) {
-            return "redirect:/dashboard";
-        } else {
-            return "redirect:/login";
-        }
-    }
-
-    @GetMapping("/managerDashboard")
-    public String managerDashboard(HttpSession session, Model model) {
-        User loggedInUser = (User) session.getAttribute("loggedInUser");
-        if (loggedInUser != null && loggedInUser.getRoleId() == 2) {
-            return "managerDashboard";
-        } else {
-            return "redirect:/dashboard";
-        }
-    }
-
-    @GetMapping("/dashboard")
-    public String dashboard(HttpSession session, Model model) {
-        User loggedInUser = (User) session.getAttribute("loggedInUser");
-        if (loggedInUser != null) {
-            return "dashboard";
-        } else {
-            return "redirect:/login";
-        }
-    }
-
-    @GetMapping("/createUserForm")
-    public String showCreateUserForm(HttpSession session, Model model) {
-        User loggedInUser = (User) session.getAttribute("loggedInUser");
-        if (loggedInUser != null && loggedInUser.getRoleId() == 1) {
-            model.addAttribute("user", new User());
-            return "createUser";
-        }
-        return "redirect:/dashboard";
-    }
-
-    @PostMapping("/createUser")
-    public String createUser(HttpSession session, @ModelAttribute("user") User newUser) throws SQLException {
-        User loggedInUser = (User) session.getAttribute("loggedInUser");
-        if (loggedInUser != null && loggedInUser.getRoleId() == 1) {
-            userService.createUser(newUser);
-            return "redirect:/allUsers";
-        }
-        return "redirect:/dashboard";
-    }
 
     @GetMapping("/createProjectForm")
     public String showCreateProjectForm(HttpSession session, Model model) {
@@ -157,23 +72,9 @@ public class ProjectController {
         return "redirect:/dashboard";
     }
 
-    @GetMapping("/logout")
-    public String logout(HttpSession session) {
-        session.invalidate();
-        return "redirect:/login"; // Redirect to the login page
-    }
 
-    @GetMapping("/managedProjects")
-    public String managedProjects(HttpSession session, Model model) throws SQLException {
-        User loggedInUser = (User) session.getAttribute("loggedInUser");
-        if (loggedInUser != null && loggedInUser.getRoleId() == 2) {
-            int managerUserId = loggedInUser.getUserId();
-            List<Project> projects = userService.findManagedProjects(managerUserId);
-            model.addAttribute("projects", projects);
-            return "managedProjects";
-        }
-        return "redirect:/dashboard";
-    }
+
+
 
     @GetMapping("/allProjects")
     public String allProjects(HttpSession session, Model model) throws SQLException {
@@ -256,16 +157,7 @@ public class ProjectController {
         return "redirect:/dashboard";
     }
 
-    @GetMapping("/allUsers")
-    public String allUsers(HttpSession session, Model model) throws SQLException {
-        User loggedInUser = (User) session.getAttribute("loggedInUser");
-        if (loggedInUser != null && loggedInUser.getRoleId() == 1) {
-            List<User> users = userService.getAllUsers();
-            model.addAttribute("users", users);
-            return "showUsers";
-        }
-        return "redirect:/dashboard";
-    }
+
     @PostMapping("/{taskId}/unassignUser")
     public String unassignUser(@PathVariable int taskId, @RequestParam int userId, HttpSession session) throws SQLException {
         User loggedInUser = (User) session.getAttribute("loggedInUser");
