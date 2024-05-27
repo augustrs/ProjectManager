b79ef3a078065432a5aa870a0cb2323bf94cf11d
augustrs <auso0001@stud.kea.dk>
2024-05-27 19:55:17 +0200

    deleted exceptions

--- src/main/java/com/example/projectmanagerkea/exceptions/DatabaseOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectmanagerkea.exceptions;
-
-public class DatabaseOperationException extends Exception {
-    public DatabaseOperationException(String message, Throwable err) {
-        super(message, err);
-    }
-}
--- src/main/java/com/example/projectmanagerkea/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectmanagerkea.exceptions;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
